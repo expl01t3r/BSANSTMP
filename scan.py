0305c3c8a7585e77a5f8ace86a538ccbecc4d3c3
Diego <expl01t3r@users.noreply.github.com>
2015-01-28 13:41:25 -0200

    Update scan.py

--- scan.py
+++ scan.py
@@ -23,13 +23,13 @@ ps = op.parse_args()
 print('[+] Inicando escaneamento\n')
 ps.alvo = ''.join((x+' ' for x in ps.alvo))
 
-result = nscan.scan(str(ps.alvo), ps.porta, '-A')
+result = nscan.scan(str(ps.alvo), ps.porta, '-A --version-all')
 for alvo in result['scan']:
     print('Resultados para o alvo: {alvo}\nNome do Computador: {pcnome}\nSistema Operacional: {so}\nIP: {ip}\nMAC: {mac}\nTécnica de escaneamento: {tecscan}\n' .format(alvo = alvo,
     pcnome = result['scan'][alvo]['hostname'], so = result['scan'][alvo]['osmatch'][0]['name'], ip = result['scan'][alvo]['addresses']['ipv4'],
     mac = result['scan'][alvo]['addresses']['mac'], tecscan = result['nmap']['scaninfo']['tcp']['method']))
     for x in result['scan'][alvo]['tcp']:
-        print('Porta: {porta}\nEstado: {estado}\nNome do Serviço: {nome}\nver do Serviço: {versao}\nInformações Extras: {extra}\n{leponto}'.format(
+        print('Porta: {porta}\nEstado: {estado}\nNome do Serviço: {nome}\nversão do Serviço: {versao}\nInformações Extras: {extra}\n{leponto}'.format(
             porta = x, estado = result['scan'][alvo]['tcp'][x]['state'], nome = result['scan'][alvo]['tcp'][x]['name'], versao = result['scan'][alvo]['tcp'][x]['version'],
             extra = result['scan'][alvo]['tcp'][x]['extrainfo'], leponto = ('-'*20)+'\n'))
         porta.append(x)
